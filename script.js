b2adab33d7f954fe8870158aa9a56ba6f05d850e
h2mil <om_msk@outlook.com>
2024-04-04 10:39:41 +0200

    added basic search function (no API)

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function displaySubmitElement(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = searchInput.value;
+}
 
+let searchElement = document.querySelector("#search-form");
+searchElement.addEventListener("submit", displaySubmitElement);

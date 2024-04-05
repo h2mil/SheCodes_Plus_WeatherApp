764d37759879001a9e8128c84ea8f34c7abb250c
h2mil <om_msk@outlook.com>
2024-04-05 14:55:10 +0200

    refactor
    city search/submit results in temperature being displayed

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+function updateTemperature(response) {
+  let currentTemperature = Math.round(response.data.temperature.current);
+  let temperatureValue = document.querySelector("#temperature-value");
+  temperatureValue.innerHTML = currentTemperature;
+}
+
+function searchCity(city) {
+  let apiKey = "308ea326edf2b15o6tb4107799647841";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(url).then(updateTemperature);
+}
+
 function displaySubmitElement(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let currentCity = document.querySelector("#current-city");
   currentCity.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchElement = document.querySelector("#search-form");
